ad88982c04cad40aeaacb453df1310d9f893c872
ecabigting <ericcabigting@outlook.com>
2022-01-12 15:11:25 +0400

    fixed login in auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const User = require('../models/User');
-const CryptoJS = require('crypto-js');
+const cryptoJS = require('crypto-js');
 const dotenv = require('dotenv');
 dotenv.config();
 
@@ -9,7 +9,7 @@ router.post('/register', async (req, res) => {
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
-    password: CryptoJS.AES.encrypt(req.body.password, process.env.SKEY).toString(),
+    password: cryptoJS.AES.encrypt(req.body.password, process.env.SKEY).toString(),
   });
 
   try {
@@ -20,21 +20,29 @@ router.post('/register', async (req, res) => {
   }
 });
 
-//TODO: FIX THE FOLLOWING CODE BELOW BROKEN
 router.post('/login', async (req, res) => {
   try {
     const user = await User.findOne({ username: req.body.username });
-    !user && res.status(401).send({ msg25: 'Invalid Credentials' });
-
-    const hashedPassword = CryptoJS.AES.decrypt(user.password, process.env.SKEY);
-    const password = hashedPassword.toString(CryptoJS.enc.utf8);
-
-    password !== req.body.password && res.status(401).send({ msg30: 'Invalid Credentials' });
-
-    res.status(200).send(user);
+    if(!user)
+    {
+      return res.status(401).json({ "msg25": 'Invalid Credentials' }); 
+    }else
+    {
+      console.log("password from db: " + user.password);
+      const decryptedPass = cryptoJS.AES.decrypt(user.password, process.env.SKEY).toString(cryptoJS.enc.Utf8);
+      console.log("decrypted pass: " + decryptedPass);
+      console.log("password from body: " + req.body.password);
+      if(req.body.password !== decryptedPass)
+      {
+          return res.status(401).send({ "msg30": 'Invalid Credentials' });
+      }
+      // destructure the user doc from mongodb and remove password, only return remaning properties
+      const {password,...remainingFields} = user._doc; 
+      return res.status(200).json(remainingFields);
+    }
   } catch (err) {
     console.log(err);
-    //res.status(500).json(err);
+    return res.status(500).json(err);
   }
 });
 

3ace32f0ce73ef846f9b83273938a33f7ccdae13
ecabigting <ericcabigting@outlook.com>
2022-01-13 13:54:36 +0400

    implemented jwt on login

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const User = require('../models/User');
 const cryptoJS = require('crypto-js');
 const dotenv = require('dotenv');
+const jwtoken = require('jsonwebtoken');
 dotenv.config();
 
 // REGISTER END POINT
@@ -20,25 +21,33 @@ router.post('/register', async (req, res) => {
   }
 });
 
+// LOGIN END POINT
 router.post('/login', async (req, res) => {
   try {
     const user = await User.findOne({ username: req.body.username });
-    if(!user)
-    {
-      return res.status(401).json({ "msg25": 'Invalid Credentials' }); 
-    }else
-    {
-      console.log("password from db: " + user.password);
+    if (!user) {
+      return res.status(401).json({ msg25: 'Invalid Credentials' });
+    } else {
+      console.log('password from db: ' + user.password);
       const decryptedPass = cryptoJS.AES.decrypt(user.password, process.env.SKEY).toString(cryptoJS.enc.Utf8);
-      console.log("decrypted pass: " + decryptedPass);
-      console.log("password from body: " + req.body.password);
-      if(req.body.password !== decryptedPass)
-      {
-          return res.status(401).send({ "msg30": 'Invalid Credentials' });
+      console.log('decrypted pass: ' + decryptedPass);
+      console.log('password from body: ' + req.body.password);
+      if (req.body.password !== decryptedPass) {
+        return res.status(401).send({ msg30: 'Invalid Credentials' });
       }
+      //create a json web token for the user
+      const accessToken = jwtoken.sign(
+        {
+          id: user._id.toString(),
+          isAdmin: user.isAdmin,
+        },
+        process.env.JWTKEY,
+        { expiresIn : "3d"}
+      );
+
       // destructure the user doc from mongodb and remove password, only return remaning properties
-      const {password,...remainingFields} = user._doc; 
-      return res.status(200).json(remainingFields);
+      const { password, ...remainingFields } = user._doc;
+      return res.status(200).json({...remainingFields,accessToken});
     }
   } catch (err) {
     console.log(err);

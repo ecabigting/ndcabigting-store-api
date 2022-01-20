ba6bd986180e4063e223e9d3cf9a5364ac2a0b20
ecabigting <ericcabigting@outlook.com>
2022-01-20 19:23:16 +0400

    added cart routes

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const userRoute = require("./routes/users")
-const authRoute = require("./routes/auth")
-const productRoute = require("./routes/product")
+const userRoute = require('./routes/users');
+const authRoute = require('./routes/auth');
+const productRoute = require('./routes/product');
+const cartRoute = require('./routes/cart');
 
 dotenv.config();
 
@@ -19,6 +20,7 @@ app.listen(process.env.PORT || 5000, () => {
 
 app.use(express.json());
 
-app.use("/api/users",userRoute);
-app.use("/api/auth",authRoute);
-app.use("/api/products",productRoute);
+app.use('/api/users', userRoute);
+app.use('/api/auth', authRoute);
+app.use('/api/products', productRoute);
+app.use('/api/cart', cartRoute);
--- routes/cart.js
+++ routes/cart.js
@@ -1,5 +1,73 @@
-const router = require("express").Router();
+const { verifyToken, verifyTokenAuthorization, verifyTokenIsAdmin } = require('./verifyToken');
+const Cart = require('../models/Cart');
+const { find } = require('../models/Cart');
+const router = require('express').Router();
 
+// CREATE CART
+router.post("/",verifyToken, async (req,res)=>{
+    const newCart = new Cart(req.body);
+    try
+    {
+        const savedCart = await newCart.save();
+        return res.status(200).json(savedCart);
+    }catch(err)
+    {
+        return res.status(500).json({msg14:"Internal Server Error!",err});
+    }
+});
 
+// UPDATE CART
+router.put("/:id",verifyTokenAuthorization,async(req,res)=>{
+    try
+    {
+        const updatedCart = await Cart.findByIdAndUpdate(
+            req.params.id,
+            {
+                $set:req.body
+            },
+            {new:true}
+        );
+        return res.status(200).json(updatedCart);
+    }catch(err)
+    {
+        return res.status(500).json({msg32:"Internal Server Error!",err});
+    }
+});
+
+// DELETE CART
+router.delete("/:id",verifyTokenAuthorization,async (req,res)=>{
+    try
+    {
+        await Cart.findByIdAndDelete(req.params.id);
+        return res.json(200).json({msg41:"Cart with ID: " + req.params.id + " has been deleted!"});
+    }catch(err)
+    {
+        return res.status(500).json({msg43:"Internal Server Error!",err});
+    }
+});
+
+// GET USER CART
+router.get("/find/:userId",verifyTokenAuthorization,async(req,res)=>{
+    try
+    {
+        const cart = await Cart.findOne({usuerId: req.params.userId});
+        return res.status(200).json(cart);
+    }catch(err)
+    {
+        return res.status(500).json({msg57:"Internal Server Error!",err});
+    }
+});
+
+// GET ALL CARTS
+router.get("/",verifyTokenIsAdmin,async(req,res)=>{
+    try
+    {
+        const carts = await Cart.find();
+        return res.status(200).json(carts);
+    }catch(err)
+    {
+        return res.status(500).json({msg69:"Internal Server Error!",err});
+    }
+});
 
 module.exports = router;
